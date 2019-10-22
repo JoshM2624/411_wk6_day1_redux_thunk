8485a107583c44ac46d8778b530ed5717bb907d0
Joshua Martinez <jam@Joshuas-MacBook-Pro.local>
2019-10-21 20:47:28 -0500

    first push

--- src/components/Import.js
+++ src/components/Import.js
@@ -1,11 +1,61 @@
-import React from 'react'
+import React from 'react';
+import { Container, Table, TableBody, TableCell, TableHead, TableRow, Button, Menu, MenuItem } from '@material-ui/core';
+import { MoreVert } from '@material-ui/icons';
 
 const Import = (props) => {
-    // fill out this component
+	// fill out this component
+	const [ anchorEl, setAnchorEl ] = React.useState(null);
+	const open = Boolean(anchorEl);
 
-    return (
-        <p>Import Component</p>
-    )
-}
+	const handleClick = (event) => {
+		setAnchorEl(event.currentTarget);
+	};
 
-export default Import
+	const handleClose = () => {
+		setAnchorEl(null);
+	};
+	const options = [ 'delete' ];
+
+	return (
+		<Container>
+			<p>Import Component</p>
+			<div style={{ textAlign: 'center' }}>
+				<Button variant="contained" className="import" onClick={props.fetchMakes}>
+					Import
+				</Button>
+			</div>
+
+			<Table>
+				<TableHead>
+					<TableRow>
+						<TableCell>Id</TableCell>
+						<TableCell>Make</TableCell>
+						<TableCell>Actions</TableCell>
+					</TableRow>
+				</TableHead>
+				<TableBody>
+					{props.makes.map((make, idx) => (
+						<TableRow key={make.id}>
+							<TableCell component="th" scope="row">
+								{make.MakeId}
+							</TableCell>
+							<TableCell>{make['MakeName']}</TableCell>
+							<TableCell>
+								<MoreVert onClick={handleClick} />
+							</TableCell>
+						</TableRow>
+					))}
+				</TableBody>
+			</Table>
+			<Menu id="long-menu" anchorEl={anchorEl} keepMounted open={open} onClose={handleClose}>
+				{options.map((option) => (
+					<MenuItem key={option} selected={option === 'Pyxis'} onClick={handleClose}>
+						{option}
+					</MenuItem>
+				))}
+			</Menu>
+		</Container>
+	);
+};
+
+export default Import;
